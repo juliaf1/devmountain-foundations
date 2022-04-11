5478b4da7efada2d0e922e8d76634c598c13bf50
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-11 11:28:39 -0300

    Adds other solution to calculator exercise

--- unit1/calculator/calc.js
+++ unit1/calculator/calc.js
@@ -12,39 +12,71 @@ reader.question("What would you like to calculate?", function(input){
 	// Gets math symbol (first element of user input)
 	mathSymbol = tokens[0];
 
-	// Removes first item (math operator) from array
-	tokens.shift();
+	// Create array of numbers from user input
+	numbers = getNumbers(tokens);
 
 	// Gets result
-	result = getResult(mathSymbol, tokens);
+	result = getResult(mathSymbol, numbers);
 
 	// Prints result
 	printResult(result);
 
 	// This line closes the connection to the command line interface.
-	reader.close()
+	reader.close();
 });
 
+const getNumbers = (array) => {
+	// Removes first item (math operator) from array
+	array.shift();
+
+	// Iterates over every element on array, replacing it with Number format
+	array.forEach((n) => {
+		array.splice(array.indexOf(n), 1, Number(n));
+	});
+
+	return array;
+}
+
 const getResult = (operator, operands) => {
-	if (operator == '√') {
-		result = '';
-		operands.forEach((o) => {
-			result += `${o} => ${Math.sqrt(o)}; `;
-		});
-	} else {
-		result = operands[0];
-		for(let i = 1; i < operands.length; i++){
-			result += `${operator + operands[i]}`;
-		};
-		result = eval(result);
+	result = operands[0];
+
+	switch (operator) {
+		case '+':
+			for(var i = 1; i < operands.length; i++){
+				result += operands[i];
+			};
+			return result;
+		case '-':
+			for(var i = 1; i < operands.length; i++){
+				result -= operands[i];
+			};
+			return result;
+		case '%':
+			for(var i = 1; i < operands.length; i++){
+				result %= operands[i];
+			};
+			return result;
+		case '/':
+			for(var i = 1; i < operands.length; i++){
+				result /= operands[i];
+			};
+			return result;
+		case '*':
+			for(var i = 1; i < operands.length; i++){
+				result *= operands[i];
+			};
+			return result;
+		case 'square', '√':
+			squareRoots = '';
+			operands.forEach((o) => {
+				squareRoots += `${o} => ${Math.sqrt(o)}; `;
+			});
+			return squareRoots;
+		default:
+			return 'Ops, could not find the result for that one';
 	};
-	return result
 };
 
 const printResult = (result) => {
-	if (result == undefined) {
-		console.log('Ops, could not find the result for that one');
-	} else {
-		console.log(`The result is: ${result}`);
-	};
+	console.log(`The result is: ${result}`);
 };
