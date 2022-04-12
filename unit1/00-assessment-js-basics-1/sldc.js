161b01c96a0d6b81f225273022fe1df25610eeac
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-11 21:00:55 -0300

    Includes answers for unit1/assessment dev lifecycle  exercise

--- unit1/00-assessment-js-basics-1/sldc.js
+++ unit1/00-assessment-js-basics-1/sldc.js
@@ -17,9 +17,10 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
+
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysisOfRequirements = "This is the seconnd step, in which "
-const design = ""
-const implementation = ""
-const testingAndIntegration = ""
-const maintain = ""
+const analysisOfRequirements = "This is the seconnd step, in which business and data models are discussed and refined"
+const design = "The third step is focused overall on user experience. This is when the user interface and user journey are designed"
+const implementation = "Developers will develop, aka code the software"
+const testingAndIntegration = "Developers will integrate features to an environment users can have access and QAs will test"
+const maintain = "Support, fix bugs, maintain code and hosting service, upgrade dependencies"
