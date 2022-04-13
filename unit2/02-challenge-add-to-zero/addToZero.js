5877ff306882cb8818bd8d2c782094a57d48bf99
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-13 14:05:34 -0300

    Adds solution to unit2/add-to-zero challenge

--- unit2/02-challenge-add-to-zero/addToZero.js
+++ unit2/02-challenge-add-to-zero/addToZero.js
@@ -2,3 +2,22 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+const addToZero = array => {
+  let adds = false;
+
+  array.forEach((item) => {
+    for(let i = 0; i < array.length; i++) {
+      if (array.indexOf(item) != i && Math.abs(array[i]) === Math.abs(item)) {
+        adds = true;
+        break;
+      };
+    };
+  });
+
+  return adds;
+};
+
+
+let arrayAddsToZero = addToZero(array);
+console.log(arrayAddsToZero);
