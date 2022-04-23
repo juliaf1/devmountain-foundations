5d9a613622d699262783dea80ee4f3b65e4b3d8a
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-23 14:38:37 -0300

    Implements solution for uni2/has-unique-chars challenge

--- unit2/07-challenge-has-unique-char/hasUniqueChars.js
+++ unit2/07-challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,19 @@
 // Write your code below
+
+const hasUniqueChars = word => {
+  let unique = true;
+  let array = word.replace(' ', '').split('');
+  array.forEach((item) => {
+    for(let i = 0; i < array.length; i++) {
+      if (array.indexOf(item) != i && array[i] === item) {
+        unique = false;
+        break;
+      };
+    };
+  });
+
+  return unique;
+};
+
+console.log(hasUniqueChars('Julia'));
+console.log(hasUniqueChars('Juliaa'));
