37c5854259ccba52e370e8f95a36c9cb25a34202
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-23 14:42:28 -0300

    Implements solution for uni2/is-palindrome challenge

--- unit2/08-challenge-is-palindrome/isPalindrome.js
+++ unit2/08-challenge-is-palindrome/isPalindrome.js
@@ -1,1 +1,9 @@
 // Write your code below
+
+const isPalindrome = word => {
+  let reversedWord = word.split('').reverse().join('');
+  return reversedWord === word;
+};
+
+console.log(isPalindrome('noon'));
+console.log(isPalindrome('hello'));
