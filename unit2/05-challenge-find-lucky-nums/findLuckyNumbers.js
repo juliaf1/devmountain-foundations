d313b270675990d63b680cbe6e78088c4a32bb5f
Julia Gomes <magalhaesjulia@id.uff.br>
2022-04-23 14:18:19 -0300

    Implements solution for uni2/find-lucky-numbers challenge

--- unit2/05-challenge-find-lucky-nums/findLuckyNumbers.js
+++ unit2/05-challenge-find-lucky-nums/findLuckyNumbers.js
@@ -1,1 +1,28 @@
 // Write your code below this line.
+
+const findLuckyNumbers = totalNumbers => {
+  // Initializes random array as an empty array
+  let randomArray = []
+  // Define 1-10 array from where we will get the random numbers
+  let oneToTenArray = [...Array(11).keys()];
+  oneToTenArray.shift();
+  // Iterates the total amount of numbers wanted in the random array
+  for (let i = 0; i < totalNumbers; i++) {
+    // Gets a random index from 0 to 10
+    let randomIndex = Math.random() * oneToTenArray.length | 0;
+    // Gets the number for that index from the 1-10 array
+    let randomNumber = oneToTenArray[randomIndex];
+    // Removes that number from the array to avoid duplicates
+    oneToTenArray.splice(randomIndex, 1);
+    // Push random number to random array
+    randomArray.push(randomNumber);
+  };
+  return randomArray;
+};
+
+console.log(findLuckyNumbers(0));
+console.log(findLuckyNumbers(2));
+console.log(findLuckyNumbers(3));
+console.log(findLuckyNumbers(6));
+console.log(findLuckyNumbers(8));
+console.log(findLuckyNumbers(10));
