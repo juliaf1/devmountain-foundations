1f0ba37c15ac7d169c0dcc4e97e4161812dc1d67
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-14 15:32:47 -0300

    feat(unit3-stringDecoder): (adds decoder function)

---
+++ unit3/06-challenge-string-decoder/stringDecoder.js
@@ -0,0 +1,18 @@
+const decoder = code => {
+  let decryptedCode = '';
+  let index = 0;
+  let skipCount = code[index];
+  while (skipCount !== undefined) {
+    index += parseInt(skipCount) + 1;
+    decryptedCode = decryptedCode.concat(code[index]);
+    index++;
+    skipCount = code[index];
+  };
+  return decryptedCode;
+};
+
+console.log(decoder('0h2xce5ngbrdy'));
+console.log(decoder('3vdfn'));
+console.log(decoder('0r'));
+console.log(decoder('2bna0p1mp2osl0e'));
+console.log(decoder('0y4akjfe0s'));
