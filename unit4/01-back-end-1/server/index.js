ff7cd11477dbb1b72783ed6e1ad34117d6e97fc6
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-21 20:37:42 -0300

    feat(unit4-backend1): (set up weather and users endpoints on index js)

--- unit4/01-back-end-1/server/index.js
+++ unit4/01-back-end-1/server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const { getUsers } = require('./controllers/users_controller');
+const { getWeather } = require('./controllers/weather_controller');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", getUsers);
+
+app.get("/weather/:temperature", getWeather);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is up on ${PORT}`));
