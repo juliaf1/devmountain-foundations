7ddf6c50cd185d432d2fea5b762d60b3622c4171
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-22 18:03:39 -0300

    feat(unit4-tshirtSorter): (adds solution to shirt sorter challenge)

--- unit4/09-chal-tshirt-sorter/tshirtSorter.js
+++ unit4/09-chal-tshirt-sorter/tshirtSorter.js
@@ -1,1 +1,19 @@
-// Write your solution below:
+const shirtSorter = string => {
+  let sortedString = []
+  string.replaceAll(' ', '').split('').forEach(size => {
+    if (size === 's') {
+      sortedString.splice(0, 0, size);
+    } else if (size === 'l') {
+      sortedString.splice(string.length, 0, size);
+    } else if (size === 'm') {
+      index = sortedString.findIndex(char => char === 'l') - 1;
+      sortedString.splice(index, 0, size);
+    };
+  });
+  return sortedString.join('');
+};
+
+
+console.log(shirtSorter('slsmmsllsmsmlmsls'));
+console.log(shirtSorter('lms'));
+console.log(shirtSorter('smllms'));
