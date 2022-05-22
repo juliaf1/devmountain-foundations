2332649598084b80e4fa2c341ef0fefc0553ce55
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-22 15:41:51 -0300

    feat(unnit4-backend2): (adds houses controllers module structure)

---
+++ unit4/03-back-end-2/server/controllers/houses_controller.js
@@ -0,0 +1,22 @@
+const getHouses = (req, res) => {
+
+};
+
+const createHouse = (req, res) => {
+
+};
+
+const updateHouse = (req, res) => {
+
+};
+
+const deleteHouse = (req, res) => {
+
+};
+
+module.exports = {
+  getHouses,
+  createHouse,
+  updateHouse,
+  deleteHouse,
+};
