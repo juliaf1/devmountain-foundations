efc9315c35b00e022271658b935cdfd9156a01c0
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-22 16:40:34 -0300

    feat(unit4-decodeString): (adds solution to decoder challenge)

--- unit4/02-challenge-decode-string/decodeString.js
+++ unit4/02-challenge-decode-string/decodeString.js
@@ -1,1 +1,23 @@
-// Write your code below this line
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const decode = code => {
+  let skipCount = +code[0];
+
+  if (!skipCount) {
+    console.log('Invalid code');
+    return
+  };
+
+  let decodedString = '';
+  let codeArray = code.substring(1).toLowerCase().split('');
+
+  codeArray.forEach(letter => {
+    decodedString += ALPHABET[skipCount + ALPHABET.findIndex(char => char === letter)];
+  });
+
+  console.log(decodedString);
+};
+
+decode('1a');
+decode('3ce');
+decode('2fcjjm');
