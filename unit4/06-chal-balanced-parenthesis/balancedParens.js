0aac8e82cf85bb615956e7d9b6874384e4dffcc2
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-22 17:37:22 -0300

    feat(unit4-balancedParenthesis): (adds solution to balanced parenthesis challenge)

--- unit4/06-chal-balanced-parenthesis/balancedParens.js
+++ unit4/06-chal-balanced-parenthesis/balancedParens.js
@@ -3,5 +3,27 @@ let sample1 = "This ( is unbalanced."
 let sample2 = "(This (is (a) balanced) string.)"
 let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
+let sample5 = ")(U)(nbalanced()"
 
 // Write your solution below:
+const hasBalancedParens = string => {
+  string = string.replaceAll(' ', '').split('');
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '(') {
+      for (let j = i + 1; j < string.length; j++) {
+        if (string[j] === ')') {
+          string.splice(j, 1);
+          string.splice(i, 1);
+          break;
+        };
+      };
+    };
+  };
+  return string.findIndex(char => char === '(' || char === ')') === -1;
+};
+
+console.log(hasBalancedParens(sample1));
+console.log(hasBalancedParens(sample2));
+console.log(hasBalancedParens(sample3));
+console.log(hasBalancedParens(sample4));
+console.log(hasBalancedParens(sample5));
