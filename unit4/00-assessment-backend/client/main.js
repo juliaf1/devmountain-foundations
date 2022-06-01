547770069cab12174027cacd420def07d46c27a9
Julia Frederico <magalhaesjulia@id.uff.br>
2022-05-31 23:08:32 -0300

    feat(unit4-assessment): (adds backend and frontend for get fortune feature)

--- unit4/00-assessment-backend/client/main.js
+++ unit4/00-assessment-backend/client/main.js
@@ -1,11 +1,23 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
+const fortuneBtn = document.getElementById("fortuneButton");
+
+const API_BASE_URL = 'http://localhost:4000/api/';
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
+    axios.get(API_BASE_URL + 'compliment')
         .then(res => {
             const data = res.data;
             alert(data);
     });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+const getFortune = () => {
+    axios.get(API_BASE_URL + 'fortune')
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+complimentBtn.addEventListener('click', getCompliment);
+fortuneBtn.addEventListener('click', getFortune);
--- unit4/00-assessment-backend/server/controller.js
+++ unit4/00-assessment-backend/server/controller.js
@@ -1,13 +1,18 @@
+const getCompliment = (req, res) => {
+    const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+    res.status(200).send(compliments.sample());
+};
+
+const getFortune = (req, res) => {
+    const fortunes = ['Bide your time, for success is near.', 'Change is happening in your life, so go with the flow!', 'Dedicate yourself with a calm mind to the task at hand.', 'Don’t be discouraged, because every wrong attempt discarded is another step forward.', 'Education is the ability to meet life’s situations.', 'It’s time to get moving. Your spirits will lift accordingly.'];
+    res.status(200).send(fortunes.sample());
+};
+
+Array.prototype.sample = function(){
+    return this[Math.floor(Math.random()*this.length)];
+};
+
 module.exports = {
-
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
-
+    getCompliment,
+    getFortune,
 }
