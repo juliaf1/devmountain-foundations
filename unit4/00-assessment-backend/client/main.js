49d0fe7dbd96d60f61f4bcd5960bfd08d3ce8648
Julia Frederico <magalhaesjulia@id.uff.br>
2022-06-01 23:42:39 -0300

    feat(unit4-assessment): (updates client side drink bot handling and styling)

--- unit4/00-assessment-backend/client/main.js
+++ unit4/00-assessment-backend/client/main.js
@@ -1,12 +1,15 @@
+const API_BASE_URL = 'http://localhost:4000/api/';
+
 const complimentBtn = document.getElementById("complimentButton");
 const fortuneBtn = document.getElementById("fortuneButton");
 const drinkBtn = document.getElementById("drinkBotButton");
+
 const sweetAlertContainer = document.querySelector(".sweet-alert-container");
-const sweetAlertText = sweetAlertContainer.querySelector("h1");
+const sweetAlertText = sweetAlertContainer.querySelector(".alert-text");
+const sweetAlertIcon = sweetAlertContainer.querySelector(".alert-icon");
+
 const toDoInput = document.querySelector("#toDoInput");
 const toDoBtn = document.querySelector("#toDoButton");
-
-const API_BASE_URL = 'http://localhost:4000/api/';
 
 const getCompliment = () => {
     axios.get(API_BASE_URL + 'compliment')
@@ -29,7 +32,8 @@ const getDrinkBotAnswer = () => {
         .then(res => {
             const data = res.data;
             sweetAlertContainer.classList.remove('d-none');
-            sweetAlertText.innerText = data;
+            sweetAlertText.innerText = data.text;
+            sweetAlertIcon.innerText = data.emoji;
             setTimeout(function() {
                 sweetAlertContainer.classList.add('d-none');
             }, 1800);
@@ -42,7 +46,8 @@ const addToDoList = () => {
         .then(res => {
             const data = res.data;
             sweetAlertContainer.classList.remove('d-none');
-            sweetAlertText.innerText = data;
+            sweetAlertText.innerText = data.text;
+            sweetAlertIcon.innerText = data.emoji;
             setTimeout(function() {
                 sweetAlertContainer.classList.add('d-none');
             }, 1800);
