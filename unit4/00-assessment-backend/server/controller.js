6e018b0a5945b366869d8a7fc946f9b87c679e20
Julia Frederico <magalhaesjulia@id.uff.br>
2022-06-01 23:31:17 -0300

    refactor(unit4-assessment): (moves server content to db.json)

--- unit4/00-assessment-backend/server/controller.js
+++ unit4/00-assessment-backend/server/controller.js
@@ -1,24 +1,22 @@
-const db = require('./db.json');
+const { toDoList, fortunes, compliments, drinkBotMessages } = require('./db.json');
 let id = 4;
 
 const getCompliment = (req, res) => {
-    const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
     res.status(200).send(compliments.sample());
 };
 
 const getFortune = (req, res) => {
-    const fortunes = ['Bide your time, for success is near.', 'Change is happening in your life, so go with the flow!', 'Dedicate yourself with a calm mind to the task at hand.', 'Don’t be discouraged, because every wrong attempt discarded is another step forward.', 'Education is the ability to meet life’s situations.', 'It’s time to get moving. Your spirits will lift accordingly.'];
     res.status(200).send(fortunes.sample());
 };
 
 const getDrinkBotAnswer = (req, res) => {
     let date = new Date();
     if (date.getDay() === 5 && date.getHours() > 18) {
-        res.status(200).send(`Yas! Time to listen to Zeca Pagodinho and drink beer 💃🍻`);
+        res.status(200).send(drinkBotMessages.fridayAfterHours);
     } else if (date.getDay() === 4) {
-        res.status(200).send(`Almost there, friday is coming 💫`);
+        res.status(200).send(drinkBotMessages.thursday);
     } else {
-        res.status(200).send(`Nope, keep working 😮‍💨`);
+        res.status(200).send(drinkBotMessages.workingHours);
     };
 };
 
@@ -26,26 +24,26 @@ const createToDoItem = (req, res) => {
     const { title } = req.body;
     db.push({ id, title });
     id++;
-    res.status(201).send(db);
-}
+    res.status(201).send(toDoList);
+};
 
 const listToDoItems = (req, res) => {
-    res.status(200).send(db);
+    res.status(200).send(toDoList);
 };
 
 const updateToDoItem = (req, res) => {
     const item = findToDoItem(req.params.id);
     item.title = req.body.title;
-    res.status(200).send(db);
+    res.status(200).send(toDoList);
 };
 
 const findToDoItemIndex = id => {
-    return db.findIndex(house => house.id == id);
-  };
+    return toDoList.findIndex(item => item.id == id);
+};
   
-  const findToDoItem = id => {
-    return db[findToDoItemIndex(id)];
-  };
+const findToDoItem = id => {
+    return toDoList[findToDoItemIndex(id)];
+};
 
 Array.prototype.sample = function(){
     return this[Math.floor(Math.random()*this.length)];
