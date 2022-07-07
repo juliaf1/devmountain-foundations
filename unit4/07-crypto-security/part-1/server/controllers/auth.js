1e669e7c5c0089fa1d47bbef370445921a8a731d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-07 14:46:14 -0300

    feat(unit7-crypto): (compares sync user password on login endpoint)

--- unit4/07-crypto-security/part-1/server/controllers/auth.js
+++ unit4/07-crypto-security/part-1/server/controllers/auth.js
@@ -3,24 +3,26 @@ const users = [];
 
 module.exports = {
     login: (req, res) => {
-      console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
-        }
-      }
-      res.status(400).send("User not found.")
+      console.log('Logging In User');
+      console.log(users);
+      console.log(req.body);
+      const { username, password } = req.body;
+      const user = users.filter(user => user.username == username)[0];
+      const correctInput = bcrypt.compareSync(password, user.password);
+      if (correctInput) {
+        res.status(200).send(user);
+      } else {
+        res.status(400).send("User not found");
+      };
     },
     register: (req, res) => {
         console.log('Registering User')
         const { password } = req.body;
-        const salt = bcrypt.genSaltSync(5) // adds extra characters to hash
-        const hashedPass = bcrypt.hashSync(password, salt); // hashes pass passing salt as argument
+        const salt = bcrypt.genSaltSync(5);
+        const hashedPass = bcrypt.hashSync(password, salt);
         req.body.password = hashedPass;
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+        console.log(req.body);
+        users.push(req.body);
+        res.status(200).send(req.body);
     }
 }
