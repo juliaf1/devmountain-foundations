328adfb5c731111aa77c888f03ca3efd0ba6ad05
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-07 14:36:56 -0300

    feat(unit7-crypt): (includes password hashing in register endpoint)

--- unit4/07-crypto-security/part-1/server/controllers/auth.js
+++ unit4/07-crypto-security/part-1/server/controllers/auth.js
@@ -15,6 +15,10 @@ module.exports = {
     },
     register: (req, res) => {
         console.log('Registering User')
+        const { password } = req.body;
+        const salt = bcrypt.genSaltSync(5) // adds extra characters to hash
+        const hashedPass = bcrypt.hashSync(password, salt); // hashes pass passing salt as argument
+        req.body.password = hashedPass;
         console.log(req.body)
         users.push(req.body)
         res.status(200).send(req.body)
