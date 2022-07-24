b837e29f4905691c0ded9e8da97574ddab3d5e5d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-24 18:34:42 -0300

    feat(unit6-asessment): (adds cors + serving public files with middleware)

--- unit6/00-assessment-qa-devops/server.js
+++ unit6/00-assessment-qa-devops/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const {bots, playerRecord} = require('./data');
 const {shuffleArray} = require('./utils');
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/styles', express.static(path.join(__dirname, './public/index.css')));
+app.use('/js', express.static(path.join(__dirname, './public/index.js')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
-app.get('/styles', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.css'));
-});
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.css'));
+// });
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.js'));
-});
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.js'));
+// });
 
 app.get('/api/robots', (req, res) => {
     try {
