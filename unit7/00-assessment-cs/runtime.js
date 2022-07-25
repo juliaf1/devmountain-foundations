fd8e7d24dcb146523147e63ec10d2f2769505cda
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-25 14:40:21 -0300

    feat(unit7-assessment): (fixes bug + adds answers to big o questions)

--- unit7/00-assessment-cs/runtime.js
+++ unit7/00-assessment-cs/runtime.js
@@ -45,32 +45,50 @@ for (const key in arrays) {
     doublerAppend(arrays[key]);
     let resultsAppend = perf.stop();
 
+    console.log(resultsAppend);
+
     perf.start();
-    doublerInsert(extraLargeArray);
+    doublerInsert(arrays[key]);
     let resultsInsert = perf.stop();
 
-    console.log('insert', resultsInsert.preciseWords);
+    console.log(resultsInsert);
+
     console.log('append', resultsAppend.preciseWords);
+    console.log('insert', resultsInsert.preciseWords);
 
     console.log('\n----\n');
 };
 
+
 // Results for the tinyArray
-// insert 775.143459 ms
-// append 47.292 μs
+// append 48.5 μs
+// insert 19.333 μs
 
 // Results for the smallArray
-// insert 766.73225 ms
-// append 12.875 μs
+// append 9.125 μs
+// insert 10.625 μs
 
 // Results for the mediumArray
-// insert 770.876792 ms
-// append 33.209 μs
+// append 50.208 μs
+// insert 139.25 μs
 
 // Results for the largeArray
-// insert 767.111041 ms
-// append 281.292 μs
+// append 425.667 μs
+// insert 8.83675 ms
 
 // Results for the extraLargeArray
-// insert 768.228333 ms
-// append 1.902 ms
+// append 2.273 ms
+// insert 772.099666 ms
+
+
+// Question - How does each function “scale”? Which of the two functions scales better? How can you tell?
+// Answer - doublerAppend runs in a O(n) time, while doublerInsert runs in O(n2) time.
+// This means doublerAppend will scale linearly, while doublerInsert will scale quadratically.
+// The asymptotic behavior of both functions can be concluded from the time results.
+
+
+// Question - Do some review / research on why the slower function is so slow, and summarize the reasoning for this.
+// Answer - the array unshift() method adds a new element to the beginning of the array.
+// To do that, as the name indicates, all the other elements of the array need to be shifted to the right.
+// This means that the time complexity of the method unshift() by itself is linear O(n).
+// Considering the function already has a foor loop with a O(n) complexity, this makes the function run in O(n2) time.
