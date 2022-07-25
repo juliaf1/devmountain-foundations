5571f4cf87e0a7e13ff8fd46f547c7b16fc12b9b
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-25 00:07:23 -0300

    feat(unit6-assessment): (adds unit tests to shuffle array function)

--- unit6/00-assessment-qa-devops/functions.test.ts
+++ unit6/00-assessment-qa-devops/functions.test.ts
@@ -1,5 +1,37 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
-})
+    const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    // check that shuffleArray returns an array
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(testArray))).toBe(true);
+    });
+
+    // check that it returns an array of the same length as the argument sent in
+    test('return an array of the same length', () => {
+        const arrayLength = testArray.length;
+        const resultLength = shuffleArray(testArray).length;
+        expect(arrayLength === resultLength).toBe(true);
+    });
+
+    // check that all the same items are in the array
+    test('return an array with all the same items', () => {
+        let matches = 0;
+        const resultArray = shuffleArray(testArray);
+        for(let i = 0; i < testArray.length; i++) {
+            for(let j = i + 1; j < resultArray.length; j++) {
+                if (testArray[i] == resultArray[j]) {
+                    matches++;
+                };
+            };
+        };
+        expect(matches === testArray.length).toBe(true);
+    });
+
+
+    // check that the items have been shuffled around
+    test('return an array with the items shuffled', () => {
+        expect(testArray !== shuffleArray(testArray)).toBe(true);
+    });
+});
