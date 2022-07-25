4970c403c83beabc9bef5e3b35c1b0abb7d2b832
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-25 10:10:20 -0300

    feat(unit6-assessment): (adds remove from duo test + refactors automation tests)

--- unit6/00-assessment-qa-devops/duelDuo.test.ts
+++ unit6/00-assessment-qa-devops/duelDuo.test.ts
@@ -6,6 +6,8 @@ const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
     await driver.get('http://localhost:3000/');
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(2000);
 });
 
 afterAll(async () => {
@@ -21,36 +23,30 @@ test('Title shows up when page loads', async () => {
 // Check that clicking the Draw button displays the div with id = “choices”
 
 test('Draw buttons displays choices div', async () => {
-    const button = await driver.findElement(By.id('draw'));
-    await button.click();
-    await driver.sleep(2000);
-    const choicesDiv = await driver.findElement(By.id('choices'));
-    const choices = await choicesDiv.findElements(By.xpath('*'));
-    expect(choices.length > 0).toBe(true);
+    const choices = await driver.findElement(By.id('choices'));
+    const displayed = await choices.isDisplayed();
+    expect(displayed).toBe(true);
 });
 
 // Check that clicking an “Add to Duo” button displays the div with id = “player-duo”
 
 test('Add to duo displays div with id player duo', async () => {
-    const drawButton = await driver.findElement(By.id('draw'));
-    await drawButton.click();
-    const button = await driver.findElement(By.xpath('//button[@class="bot-btn"]'));
-    await button.click();
+    await driver.findElement(By.xpath('//button[text()="Add to Duo"]')).click();
     await driver.sleep(2000);
-    const duoDiv = await driver.findElement(By.id('player-duo'));
-    const bots = await duoDiv.findElements(By.xpath('*'));
-    expect(bots.length > 0).toBe(true);
+    const duo = await driver.findElement(By.id('player-duo'));
+    const displayed = await duo.isDisplayed();
+    expect(displayed).toBe(true);
 });
 
 // Check that when a bot is “Removed from Duo”, that it goes back to “choices”
 
 test('Add to duo displays div with id player duo', async () => {
-    // const drawButton = await driver.findElement(By.id('draw'));
-    // await drawButton.click();
-    // const button = await driver.findElement(By.xpath('//button[@class="bot-btn"]'));
-    // await button.click();
-    // await driver.sleep(2000);
-    // const duoDiv = await driver.findElement(By.id('player-duo'));
-    // const bots = await duoDiv.findElements(By.xpath('*'));
-    // expect(bots.length > 0).toBe(true);
+    await driver.findElement(By.xpath('//button[text()="Add to Duo"]')).click();
+    await driver.sleep(2000);
+    const card = await driver.findElement(By.xpath('//div[@id="player-duo"]/div'));
+    const botName = await card.findElement(By.tagName('img')).getAttribute('alt');
+    await card.findElement(By.tagName('button')).click();
+    await driver.sleep(2000);
+    const removedDuo = await driver.findElements(By.xpath(`//div[@id="choices"]/div/img[@alt="${botName}"]`));
+    expect(removedDuo.length > 0).toBe(true);
 });
